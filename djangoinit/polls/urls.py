f3315ea513b81356790bf63e9c41d35a973e4118
sweatshirt <sweatshirtblue2@gmail.com>
2025-02-08 14:55:25 +0300

    TASK | polls index and view

--- djangoinit/polls/urls.py
+++ djangoinit/polls/urls.py
@@ -5,10 +5,12 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("id", views.view, name="view"),
     path("create", views.create, name="create"),
     path("store", views.store, name="store"),
-    path("edit", views.edit, name="edit"),
-    path("update", views.update, name="update"),
-    path("destroy", views.destroy, name="destroy"),
+    path("<int:question_id>", views.show, name="show"),
+    path("<int:question_id>/edit", views.edit, name="edit"),
+    path("<int:question_id>/update", views.update, name="update"),
+    path("<int:question_id>/results", views.result, name="result"),
+    path("<int:question_id>/vote", views.vote, name="vote"),
+    path("<int:question_id>/destroy", views.destroy, name="destroy"),
 ]
--- djangoinit/polls/views.py
+++ djangoinit/polls/views.py
@@ -1,12 +1,37 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import loader
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("The polls app.... hooray")
+    latest_questions_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_questions_list": latest_questions_list,
+    }
+    print(latest_questions_list)
+
+    return HttpResponse(template.render(context, request))
 
 
-def view(request):
-    return HttpResponse("Wanna see an item? Sikeeeee!!!")
+def show(request, question_id):
+    print(question_id)
+    # for q in Question.objects.all():
+    #     if q.id == question_id:
+    try:
+        question = Question.objects.get(pk=question_id)
+        template = loader.get_template("polls/show.html")
+        context = {
+            "question": question,
+        }
+    except Question.DoesNotExist:
+        raise Http404(f"Question with id {question_id} not found")
+    return HttpResponse(template.render(context, request))
+    # return HttpResponse(q)
+    # return HttpResponse(
+    #     f"Wanna see an item? Sikeeeee!!! No item found for id {question_id}"
+    # )
 
 
 def create(request):
@@ -17,17 +42,32 @@ def store(request):
     return HttpResponse("Ain't you tired of the sikes?")
 
 
-def edit(request):
+def edit(request, question_id):
+    print(question_id)
     return HttpResponse(
-        "You could not live with you own failure. Where did that bring you? Back to me"
+        f"You could not live with you own failure. Where did that bring you? Back to me. Question Id: {question_id}"
     )
 
 
-def update(request):
+def update(request, question_id):
+    print(question_id)
     return HttpResponse(
-        "Mr. yale, there's no need to both cuss and yell, one of them will suffice"
+        f"Mr. yale, there's no need to both cuss and yell, one of them will suffice. Question Id: {question_id}"
     )
 
 
-def destroy(request):
-    return HttpResponse("Dread it, run from it, destiny arrives all the same!")
+def destroy(request, question_id):
+    print(question_id)
+    return HttpResponse(
+        f"Dread it, run from it, destiny arrives all the same! Question Id: {question_id}"
+    )
+
+
+def vote(request, question_id):
+    print(question_id)
+    return HttpResponse(f"voting on Question {question_id}")
+
+
+def result(request, question_id):
+    print(question_id)
+    return HttpResponse(f"results for {question_id}")
