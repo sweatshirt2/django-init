e6709cdf615d6b5fb36db8e7ebd159b5c145c11b
sweatshirt <sweatshirtblue2@gmail.com>
2025-02-09 17:26:19 +0300

    finished part 4

--- djangoinit/polls/urls.py
+++ djangoinit/polls/urls.py
@@ -2,15 +2,23 @@ from django.urls import path
 
 from . import views
 
+# app_name = "polls"
+# urlpatterns = [
+#     path("", views.index, name="index"),
+#     path("create", views.create, name="create"),
+#     path("store", views.store, name="store"),
+#     path("<int:question_id>", views.show, name="show"),
+#     path("<int:question_id>/edit", views.edit, name="edit"),
+#     path("<int:question_id>/update", views.update, name="update"),
+#     path("<int:question_id>/results", views.result, name="results"),
+#     path("<int:question_id>/vote", views.vote, name="vote"),
+#     path("<int:question_id>/destroy", views.destroy, name="destroy"),
+# ]
 
+app_name = "polls"
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("create", views.create, name="create"),
-    path("store", views.store, name="store"),
-    path("<int:question_id>", views.show, name="show"),
-    path("<int:question_id>/edit", views.edit, name="edit"),
-    path("<int:question_id>/update", views.update, name="update"),
-    path("<int:question_id>/results", views.result, name="result"),
-    path("<int:question_id>/vote", views.vote, name="vote"),
-    path("<int:question_id>/destroy", views.destroy, name="destroy"),
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>", views.ShowView.as_view(), name="show"),
+    path("<int:pk>/results", views.ResultsView.as_view(), name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
--- djangoinit/polls/views.py
+++ djangoinit/polls/views.py
@@ -1,37 +1,41 @@
-from django.http import HttpResponse, Http404
+from django.db.models import F
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
+from django.shortcuts import get_object_or_404, render
+from django.views import generic
 
-from .models import Question
-
-
-def index(request):
-    latest_questions_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
-    context = {
-        "latest_questions_list": latest_questions_list,
-    }
-    print(latest_questions_list)
-
-    return HttpResponse(template.render(context, request))
+from .models import Question, Choice
 
 
-def show(request, question_id):
-    print(question_id)
-    # for q in Question.objects.all():
-    #     if q.id == question_id:
-    try:
-        question = Question.objects.get(pk=question_id)
-        template = loader.get_template("polls/show.html")
-        context = {
-            "question": question,
-        }
-    except Question.DoesNotExist:
-        raise Http404(f"Question with id {question_id} not found")
-    return HttpResponse(template.render(context, request))
-    # return HttpResponse(q)
-    # return HttpResponse(
-    #     f"Wanna see an item? Sikeeeee!!! No item found for id {question_id}"
-    # )
+# def index(request):
+#     latest_questions_list = Question.objects.order_by("-pub_date")[:5]
+#     template = loader.get_template("polls/index.html")
+#     context = {
+#         "latest_questions_list": latest_questions_list,
+#     }
+#     print(latest_questions_list)
+
+#     return HttpResponse(template.render(context, request))
+
+
+# def show(request, question_id):
+#     print(question_id)
+#     # for q in Question.objects.all():
+#     #     if q.id == question_id:
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#         template = loader.get_template("polls/show.html")
+#         context = {
+#             "question": question,
+#         }
+#     except Question.DoesNotExist:
+#         raise Http404(f"Question with id {question_id} not found")
+#     return HttpResponse(template.render(context, request))
+#     # return HttpResponse(q)
+#     # return HttpResponse(
+#     #     f"Wanna see an item? Sikeeeee!!! No item found for id {question_id}"
+#     # )
 
 
 def create(request):
@@ -64,10 +68,46 @@ def destroy(request, question_id):
 
 
 def vote(request, question_id):
-    print(question_id)
-    return HttpResponse(f"voting on Question {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "polls/show.html",
+            {
+                "question": question,
+                "error_message": "No choice selected.",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
 
 
-def result(request, question_id):
-    print(question_id)
-    return HttpResponse(f"results for {question_id}")
+# def result(request, question_id):
+#     # print(question_id)
+#     # return HttpResponse(f"results for {question_id}")
+
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/results.html", {"question": question})
+
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:10]
+
+
+class ShowView(generic.DetailView):
+    model = Question
+    template_name = "polls/show.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
